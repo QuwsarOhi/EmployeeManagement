043e64e18c48063ce19473c19daea356daf086c8
QuwsarOhi <quwsarohi@gmail.com>
2019-02-07 19:28:47 +0600

    department, role views done

--- app/auth/views.py
+++ app/auth/views.py
@@ -37,13 +37,8 @@ def register():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    """
-    Handle requests to the /login route
-    Log an employee in through the login form
-    """
     form = LoginForm()
     if form.validate_on_submit():
-
         # check whether employee exists in the database and whether
         # the password entered matches the password in the database
         employee = Employee.query.filter_by(email=form.email.data).first()
@@ -51,8 +46,11 @@ def login():
             # log employee in
             login_user(employee)
 
-            # redirect to the dashboard page after login
-            return redirect(url_for('home.dashboard'))
+            # redirect to the appropriate dashboard page
+            if employee.is_admin:
+                return redirect(url_for('home.admin_dashboard'))
+            else:
+                return redirect(url_for('home.dashboard'))
 
         # when login details are incorrect
         else:
--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,7 @@
 # app/home/views.py
 
-from flask import render_template
-from flask_login import login_required
+from flask import abort, render_template
+from flask_login import login_required, current_user
 
 from . import home
 
@@ -21,3 +21,14 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
+
+
+# add admin dashboard view
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    # prevent non-admins from accessing the page
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('home/admin_dashboard.html', title="Dashboard")
